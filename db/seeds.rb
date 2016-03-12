ae84c34e770715825910b71ca2778b49ea84c9db
Javier Forero <javierforero01@gmail.com>
2016-03-12 10:51:59 -0600

    checkpoint-30-assignment 2nd commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,14 @@ posts = Post.all
   body: RandomData.random_paragraph
   )
 end
+
 #unique post
  unique_post = Post.find_or_create_by(title: "Unique Post", body:  "I am an unique post")
 #unique comment tied to unique post
  unique_post.comments.find_or_create_by(
     body: "I am a comment in the unique post"
   )
+
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
