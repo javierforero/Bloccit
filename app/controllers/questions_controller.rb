17fd8ee9166f31ae1ccb17704c5c0f70efe6306c
Javier Forero <javierforero01@gmail.com>
2016-03-17 19:48:10 -0500

    checkpoint-34 unfinished

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -56,5 +56,4 @@ class QuestionsController < ApplicationController
       render :show
     end
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :posts, :advertisements, :questions
+  resources :posts, :advertisements, :topics, :questions
 
   get 'about' => 'welcome#about'
 
