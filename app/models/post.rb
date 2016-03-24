051821c955f857248723e321deeb698cd5d80326
Javier Forero <javierforero01@gmail.com>
2016-03-24 13:43:42 -0500

    checkpoint-39-assignment 2nd commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :user
   default_scope { order('created_at DESC') }
+  # checkpoint-39 assignment code
   scope :ordered_by_title, -> {order('title ASC')}
   scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
 
