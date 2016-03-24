fa2a36f1b3ef1887e951c8fed86cbd2ba788112c
Javier Forero <javierforero01@gmail.com>
2016-03-24 13:29:02 -0500

    checkpoint-39 2nd commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :user
   default_scope { order('created_at DESC') }
-  
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
