ec0b3d6cf1f971d3aa4cac1b86541256fe8e6091
Javier Forero <javierforero01@gmail.com>
2016-03-28 14:33:03 -0500

    checkpoint-41

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
+  has_many :comments
   before_save { self.email = email.downcase}
   before_save do
     self.name = name.split(' ').map {|name| name.capitalize }.join(' ') if name
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,9 +10,10 @@ RSpec.describe Post, type: :model do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:body) }
   it { is_expected.to validate_presence_of(:topic) }
-  it { is_expected.to validate_presence_of(:user) }  
+  it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_length_of(:title).is_at_least(5) }
   it { is_expected.to validate_length_of(:body).is_at_least(20) }
+  it { is_expected.to have_many(:comments) }
 
   describe "attributes" do
     it "responds to title" do
