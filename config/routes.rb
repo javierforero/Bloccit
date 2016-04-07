c62649d90826c3a8697c9c773057831d7bffd50f
Javier Forero <javierforero01@gmail.com>
2016-04-07 18:32:52 -0500

    checkpoint-48 in implementation

--- config/routes.rb
+++ config/routes.rb
@@ -21,12 +21,17 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :labels, only: [:show]
 
-
   get 'about' => 'welcome#about'
 
   post 'users/confirm' => 'users#confirm'
 
   root 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+    end
+  end
 
   get 'welcome/faq'
 end
