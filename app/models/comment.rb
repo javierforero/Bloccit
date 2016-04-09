daef5c5f17a6cc95440f183ba2a9b46556dac959
David Roberts <drob@drob.net>
2016-04-09 18:22:12 -0400

    make comment belong to user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
+  belongs_to :user
 
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
